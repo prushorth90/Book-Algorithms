d9422ddbda6c49961bf41088c1fbff053ace383b
Manivannan Prushorth <manivanann.prushorth@kcl.ac.uk>
2022-03-04 23:30:52 +0000

    Add knn and sim metrics to simple item collab

--- SimpleUserCollab/evaluate_simple_user_collab.py
+++ SimpleUserCollab/evaluate_simple_user_collab.py
@@ -1,5 +1,5 @@
 from book_data import BookData
-from surprise import KNNBasic
+from surprise import KNNBasic, KNNWithMeans, KNNWithZScore, KNNBaseline
 import heapq
 from collections import defaultdict
 from operator import itemgetter
@@ -21,54 +21,69 @@ evalData = EvaluationData(data, rankings)
 
 # Train on leave-One-Out train set
 trainSet = evalData.GetLOOCVTrainSet()
-sim_options = {'name': 'cosine',
-               'user_based': True
-               }
+sim_measures = {'cosine': 'cosine',
+                'msd': 'msd',
+                'pearson': 'pearson',
+                'pearson_baseline': 'pearson_baseline'
+}
 
-model = KNNBasic(sim_options=sim_options)
-model.fit(trainSet)
-simsMatrix = model.compute_similarities()
+for sim_name,sim in sim_measures.items():
+    sim_options = {'name': sim,
+                   'user_based': True
+                   }
 
-leftOutTestSet = evalData.GetLOOCVTestSet()
+    models = {'KNNBASIC': KNNBasic(sim_options=sim_options),
+              'KNNWITHMEANS': KNNWithMeans(sim_options=sim_options),
+              'KNNWITHZSCORE': KNNWithZScore(sim_options=sim_options),
+              'KNNBASELINE': KNNBaseline(sim_options=sim_options)
 
-# Build up dict to lists of (int(bookID), predictedrating) pairs
-topN = defaultdict(list)
-k = 10
-for uiid in range(trainSet.n_users):
-    # Get top N similar users to this one
-    similarityRow = simsMatrix[uiid]
+    }
+    for model_name,knnmodel in models.items():
+        model = knnmodel
+        model.fit(trainSet)
+        simsMatrix = model.compute_similarities()
 
-    similarUsers = []
-    for innerID, score in enumerate(similarityRow):
-        if (innerID != uiid):
-            similarUsers.append( (innerID, score) )
+        leftOutTestSet = evalData.GetLOOCVTestSet()
 
-    kNeighbors = heapq.nlargest(k, similarUsers, key=lambda t: t[1])
+        # Build up dict to lists of (int(bookID), predictedrating) pairs
+        topN = defaultdict(list)
+        k = 10
+        for uiid in range(trainSet.n_users):
+            # Get top N similar users to this one
+            similarityRow = simsMatrix[uiid]
 
-    # Get the stuff they rated, and add up ratings for each item, weighted by user similarity
-    candidates = defaultdict(float)
-    for similarUser in kNeighbors:
-        innerID = similarUser[0]
-        userSimilarityScore = similarUser[1]
-        theirRatings = trainSet.ur[innerID]
-        for rating in theirRatings:
-            candidates[rating[0]] += (rating[1] / 10.0) * userSimilarityScore
+            similarUsers = []
+            for innerID, score in enumerate(similarityRow):
+                if (innerID != uiid):
+                    similarUsers.append( (innerID, score) )
 
-    # Build a dictionary of stuff the user has already seen
-    watched = {}
-    for itemID, rating in trainSet.ur[uiid]:
-        watched[itemID] = 1
+            kNeighbors = heapq.nlargest(k, similarUsers, key=lambda t: t[1])
 
-    # Get top-rated items from similar users:
-    pos = 0
-    for itemID, ratingSum in sorted(candidates.items(), key=itemgetter(1), reverse=True):
-        if not itemID in watched:
-            bookID = trainSet.to_raw_iid(itemID)
-            # change int
-            topN[int(trainSet.to_raw_uid(uiid))].append( (bookID, 0.0) )
-            pos += 1
-            if (pos > 40):
-                break
+            # Get the stuff they rated, and add up ratings for each item, weighted by user similarity
+            candidates = defaultdict(float)
+            for similarUser in kNeighbors:
+                innerID = similarUser[0]
+                userSimilarityScore = similarUser[1]
+                theirRatings = trainSet.ur[innerID]
+                for rating in theirRatings:
+                    candidates[rating[0]] += (rating[1] / 10.0) * userSimilarityScore
 
-# Measure
-print("HR", RecommenderMetrics.HitRate(topN, leftOutTestSet))
+            # Build a dictionary of stuff the user has already seen
+            watched = {}
+            for itemID, rating in trainSet.ur[uiid]:
+                watched[itemID] = 1
+
+            # Get top-rated items from similar users:
+            pos = 0
+            for itemID, ratingSum in sorted(candidates.items(), key=itemgetter(1), reverse=True):
+                if not itemID in watched:
+                    bookID = trainSet.to_raw_iid(itemID)
+                    # change int
+                    topN[int(trainSet.to_raw_uid(uiid))].append( (bookID, 0.0) )
+                    pos += 1
+                    if (pos > 40):
+                        break
+
+        # Measure
+        print(f'HIT RATE: {sim_name} and {model_name}', RecommenderMetrics.HitRate(topN, leftOutTestSet))
+        print('')
